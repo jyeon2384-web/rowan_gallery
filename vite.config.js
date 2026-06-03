4f390c99a8d5d365dbcd0ce65754bd116150c5fb
이지연 <jiyeon@ijiyeon-ui-MacBookAir.local>
2026-06-03 23:34:01 +0900

    fix underscore in base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rowan-gallery/', // GitHub 저장소 이름과 일치시키세요
+  base: '/rowan_gallery/', // GitHub 저장소 이름과 일치시키세요
 })
